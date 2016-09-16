b1445b63dc023c81a4fcb443aa2fa3d30897338b
Adam Wright <adam.j.wright82@gmail.com>
2016-09-16 16:25:51 -0400

    got it so that they are all 100 - constraint 1 working

--- analyzeBioNetwork.jl
+++ analyzeBioNetwork.jl
@@ -7,6 +7,9 @@ include("pi.pl")
 
 println("Running analysis on file:")
 
+NORMAL = 100
+M = 1000
+
 if length(ARGS) == 0
     println("Please provide the path to the pairwise interaction file in the first line")
     exit()
@@ -21,7 +24,7 @@ nodes = keys(pairwiseInteractions)
    
 #nodeList is created in order to find and index for a node by name. This is used in order to make 
 #variable names e.g. X1 (X[i])
-i = 0
+
 nodesList = Array{AbstractString, 1}()
 for node in nodes
     push!(nodesList, node)
@@ -34,12 +37,37 @@ for node in nodes
         indexin([parent], nodesList) != 0 || push!(nodesList, parent)
     end
 end
-   
+
+
+# Preparing an optimization model
+m = Model(solver=GurobiSolver())
+
+# Declaring variables
+
+##node variables
+@variable(m, x[1:length(nodesList)] >=0, Int)
+
+#these are for deterining if x is above or below NORMAL
+@variable(m, xz[1:length(nodesList)], Bin)
+@variable(m, xy[1:length(nodesList)], Bin)
+
+
+###OBjective funtion
+@objective(m, Min, sum{x[i], i=1:length(nodesList)} +
+                   M*(sum{xy[i], i=1:length(nodesList)} +
+                      sum{xz[i], i=1:length(nodesList)}))
+
+##Adding constraints to 
+#@constraint(m, constraint1, -x1 +  x2 + 3x3 <= -5)
+@constraint(m, xzconst[i=1:length(nodesList)], xz[i] >= x[i] - 100)
+@constraint(m, xyconst[i=1:length(nodesList)], xy[i] >= 100 - x[i])
+
+
 for (nodeName, node) in pairwiseInteractions
     currentNodeIndex = indexin([nodeName], nodesList)
 
     if node.andOr == "AND"
-        println("AND relation")
+        #println("AND relation")
         if length(node.negParents) > 0
              #println("-- Has a negative regulator(s): ", length(node.negParents))
              #Create constraint that flips negative regulator
@@ -48,7 +76,7 @@ for (nodeName, node) in pairwiseInteractions
              #constraint 2 - take minimum from parents
         end
     else 
-        println("OR relation")
+        #println("OR relation")
         if length(node.negParents) > 0
              #println("-- Has a negative regulator(s): ", length(node.negParents))
              #Create constraint that flips negative regulator
@@ -60,3 +88,9 @@ for (nodeName, node) in pairwiseInteractions
     end 
 end
 
+solve(m)
+print(m)
+println("Optimal Solutions:")
+for i in eachindex(nodesList)
+    println( nodesList[i], " = ", getvalue(x[i]))
+end
