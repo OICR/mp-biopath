84e6d1d1086d6f14a5c752f09c9ad4b1ab3acf18
Adam Wright <adam.j.wright82@gmail.com>
2017-12-08 10:20:11 -0500

    removing data folder

--- src/expression.jl
+++ src/expression.jl
@@ -5,62 +5,19 @@ using DataFrames
 using DataTables
 using DataStructures
 
-function readExpressionFile() 
-    expressionFilename = "./data/expression-data.tsv"
-    return readtable(expressionFilename, skiprows = 5)
-end
-
-
-function hugoGeneExpression(tissueType)
-    expressionFilename = "./data/expression-data.tsv"
-
-    columns = OrderedDict("Gene ID" => String,
-                   "Gene Name" => String,
-                   "adipose tissue" => Float16,
-                   "adrenal gland" => Float16,
-                   "bone marrow" => Float16,
-                   "cerebral cortex" => Float16,
-                   "colon" => Float16,
-                   "duodenum" => Float16,
-                   "endometrium" => Float16,
-                   "esophagus" => Float16,
-                   "fallopian tube" => Float16,
-                   "gall bladder" => Float16,
-                   "heart" => Float16,
-                   "kidney" => Float16,
-                   "liver" => Float16,
-                   "lung" => Float16,
-                   "lymph node" => Float16,
-                   "ovary" => Float16,
-                   "pancreas" => Float16,
-                   "placenta" => Float16,
-                   "prostate gland" => Float16,
-                   "rectum" => Float16,
-                   "saliva-secreting gland" => Float16,
-                   "skeletal muscle tissue" => Float16,
-                   "small intestine" => Float16,
-                   "smooth muscle tissue" => Float16,
-                   "spleen" => Float16,
-                   "stomach" => Float16,
-                   "testis" => Float16,
-                   "thyroid gland" => Float16,
-                   "tonsil" => Float16,
-                   "urinary bladder" => Float16,
-                   "vermiform appendix" => Float16,
-                   "zone of skin" => Float16)
-
-    df = CSV.read(expressionFilename,
+function geneExpressionMap(filename, dataRow, tissueType)
+    # Where the Gene Name is the HUGO name
+    # The data is expected to be Float16 data
+    df = CSV.read(filename,
                   delim = "\t",
-                  header = collect(keys(columns)),
-                  types = collect(values(columns)),
-                  datarow = 5)
+                  datarow = dataRow)
 
     geneExpressionValues = Dict()
     for row in eachrow(df)
-        geneExpressionValues[row[Symbol("Gene Name")]] = row[Symbol(tissueType)]
+        geneExpressionMap[row[Symbol("Gene Name")]] = row[Symbol(tissueType)]
     end
 
-    return geneExpressionValues
+    return geneExpressionMap
 end
 
 end
